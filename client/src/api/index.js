e03794d569cd80858f10ea893dcab93f8c6aaf31
jchy <recjaswantcse@gmail.com>
2022-03-14 15:58:34 +0530

    chore : Backend has been deployed

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://lmsserverside.herokuapp.com/' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
     req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
   }
-
   return req;
 });
 
