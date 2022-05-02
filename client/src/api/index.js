7db61948f8558d0e3838bfcdd3df5f88be0f0405
Jaswant Chaudhary <91217491+jchy@users.noreply.github.com>
2022-05-02 08:20:31 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://lmsserverside.herokuapp.com/' });
+const API = axios.create({ baseURL: 'BACKEND_DEPLOYED_URL' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
